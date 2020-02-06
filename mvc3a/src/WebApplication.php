0e8b4610a0c95e9485a00d71ab806d5bdd14ba57
dr-matt-smith <matt.smith@itb.ie>
2020-02-06 10:36:52 +0000

    added project mvc3a

---
+++ mvc3a/src/WebApplication.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class WebApplication
+{
+    public function run()
+    {
+        $action = filter_input(INPUT_GET, 'action');
+
+        switch($action) {
+            case 'jokes':
+                $this->jokes();
+                break;
+
+            case 'index':
+            default:
+                $this->index();
+        }
+    }
+
+    public function index()
+    {
+        $pageTitle = 'home';
+        include __DIR__ . '/../templates/home.php';
+    }
+
+    public function jokes()
+    {
+        $pageTitle = 'jokes';
+        include __DIR__ . '/../templates/jokes.php';
+    }
+}
